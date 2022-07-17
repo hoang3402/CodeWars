6cc5ba8856e17f4ff8e4ee6a17165734c1bb37f0
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2022-07-17 11:33:55 +0000

    Done Volume of a Cuboid

---
+++ 8 kyu/Volume of a Cuboid/Kata.cs
@@ -0,0 +1,7 @@
+public class Kata
+{
+    public static double GetVolumeOfCuboid(double length, double width, double height)
+    {
+        return length * width * height;
+    }
+}
---
+++ 8 kyu/Volume of a Cuboid/main.c
@@ -0,0 +1,4 @@
+double getVolumeOfCuboid(double length, double width, double height)
+{
+    return length * width * height;
+}
