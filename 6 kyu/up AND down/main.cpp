5a645650e9eab8f2cac227640c16082155a45e22
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2022-08-19 07:48:20 +0000

    Update up AND down [main.cpp]

--- 6 kyu/up AND down/main.cpp
+++ 6 kyu/up AND down/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <map>
+#include <numeric>
 #include <string>
 
 class UpAndDown {
@@ -12,7 +13,7 @@ std::string UpAndDown::arrange(const std::string& strng)
     std::string result;
     // Create map
     std::map<std::string, int> m;
-    for (int i = 0, start = 0; i < strng.size(); ++i) {
+    for (unsigned long i = 0, start = 0; i < strng.size(); ++i) {
         if (strng[i] == ' ' || strng[i] == '\n') {
             std::string temp = strng.substr(start, i - start);
             m.insert(std::make_pair(temp, temp.length()));
@@ -22,9 +23,7 @@ std::string UpAndDown::arrange(const std::string& strng)
 
     // sort map using second element
     std::sort(m.begin(), m.end());
-    for (auto i : m) {
-        result += i.first;
-    }
+    std::accumulate(m.begin(), m.end(), result);
 
     return result;
 }
