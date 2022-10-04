7c1a0d64fd5d9ac8ce03f45f2d868c54f3c538a7
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2022-10-04 02:04:53 +0000

    Done login

---
+++ ThietKeWeb/login/js/index.js
@@ -0,0 +1,29 @@
+
+document.getElementById('username').addEventListener('keyup', function () {
+    if (document.getElementById('username').value == '') {
+        document.getElementById('error-username').style.display = 'inline';
+    } else {
+        document.getElementById('error-username').style.display = 'none';
+    }
+})
+
+document.getElementById('password').addEventListener('keyup', function () {
+    if (document.getElementById('password').value === '') {
+        document.getElementById('error-password').style.display = 'inline';
+    } else {
+        document.getElementById('error-password').style.display = 'none';
+    }
+})
+
+document.getElementById('re-password').addEventListener('keyup', function () {
+    if (document.getElementById('re-password').value === '') {
+        document.getElementById('error-re-password-null').style.display = 'inline';
+    } else {
+        document.getElementById('error-re-password-null').style.display = 'none';
+    }
+    if (document.getElementById('re-password').value != document.getElementById('password').value) {
+        document.getElementById('error-re-password-not-match').style.display = 'inline';
+    } else {
+        document.getElementById('error-re-password-not-match').style.display = 'none';
+    }
+})
