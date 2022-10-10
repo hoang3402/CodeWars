30be03ed887e86a1df344c6ec78a977c0e28b421
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2022-10-10 14:06:18 +0000

    Done

--- ThietKeWeb/lab 3/js/index.js
+++ ThietKeWeb/lab 3/js/index.js
@@ -1,15 +1,17 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+window.onload = () => {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
 
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
 }
