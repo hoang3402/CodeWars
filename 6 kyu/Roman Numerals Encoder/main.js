6115829b38ccec9a221506dc442b2cce92b474da
HoangDSR <nvh.02021995@gmail.com>
2023-11-17 22:04:20 +0700

    Done Roman Numerals Encoder

---
+++ 6 kyu/Roman Numerals Encoder/main.js
@@ -0,0 +1,25 @@
+function solution(number) {
+    let roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+    let result = '';
+    for (let i in roman) {
+        while (number >= roman[i]) {
+            result += i;
+            number -= roman[i];
+        }
+    }
+    return result;
+}
