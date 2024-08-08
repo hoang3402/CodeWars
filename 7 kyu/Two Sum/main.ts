35d4ae484538a7ab77e2f7f7b7ab935d1cde86b5
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-08-08 10:39:28 +0700

    Done Two Sum

---
+++ 7 kyu/Two Sum/main.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
